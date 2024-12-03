aeb90e991ba8ba6ec2732dc9cf677679feca0a2f
unknown <ibrahimkhan.dev405@gmail.com>
2024-12-04 00:05:24 +0600

    calculator screen functionaly done

--- lib/controllers/calculator_controller.dart
+++ lib/controllers/calculator_controller.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:math_expressions/math_expressions.dart';
 import 'package:vault/utils/colors.dart';
 
 class CalculatorController extends GetxController {
@@ -51,4 +52,46 @@ class CalculatorController extends GetxController {
 
   RxString userInput = RxString('');
   RxString result = RxString('0');
+
+  handleButton(String text) {
+    if (text == 'AC') {
+      userInput.value = '';
+      result.value = '0';
+      update();
+      return;
+    }
+    if (text == 'C') {
+      if (userInput.isNotEmpty) {
+        userInput.value =
+            userInput.value.substring(0, userInput.value.length - 1);
+        update();
+        return;
+      } else {
+        return null;
+      }
+    }
+
+    if (text == '=') {
+      var calculated = calculate();
+      if (calculated.endsWith('.0')) {
+        result.value = calculated.replaceAll('.0', '');
+        update();
+        return;
+      }
+    }
+
+    userInput.value = userInput.value + text;
+    update();
+    return;
+  }
+
+  String calculate() {
+    try {
+      var parsed = Parser().parse(userInput.value);
+      var result = parsed.evaluate(EvaluationType.REAL, ContextModel());
+      return result.toString();
+    } catch (e) {
+      return 'Syntax Error';
+    }
+  }
 }
