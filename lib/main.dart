8de9f9150bc6a979a0683ba4512fa8551c7250ec
unknown <ibrahimkhan.dev405@gmail.com>
2024-12-06 23:16:20 +0600

    calculator theme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vault/bindings/all_binding.dart';
+import 'package:vault/controllers/theme_controller.dart';
+import 'package:vault/core/services/shared_services.dart';
 import 'package:vault/utils/colors.dart';
+import 'package:vault/utils/keys.dart';
 import 'package:vault/views/SplashScreen/splash_screen.dart';
 
 void main() {
@@ -9,8 +12,29 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final themeController = Get.put(ThemeController());
+
+  checkSelectedTheme() async {
+    SharedService()
+        .getData(SetType.int, SharedKeys.selectedTheme)
+        .then((value) {
+      themeController.selectedTheme.value = value;
+    });
+  }
+
+  @override
+  void initState() {
+   checkSelectedTheme();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +42,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        scaffoldBackgroundColor: AppColors.background
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          scaffoldBackgroundColor: AppColors.background),
       home: SplashScreen(),
     );
   }
